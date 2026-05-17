431dcb4db96e422ae11d58a03f3c0a01c766bfc2
Emmanuel <emanuelalmarionovoa@gmail.com>
2026-05-17 16:12:06 -0500

    Primer commit del proyecto GArepas

---
+++ src/main/java/com/garepas/garepasapp/GarepasappApplication.java
@@ -0,0 +1,13 @@
+package com.garepas.garepasapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarepasappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarepasappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/garepas/garepasapp/GarepasappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garepas.garepasapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarepasappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
